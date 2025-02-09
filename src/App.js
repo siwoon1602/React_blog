fa1d47e7dacc175fcb1124fb86ee02cbbe848105
siwoon1602 <siwoon1602@gmail.com>
2025-02-09 12:04:09 +0900

    🎨 복습

--- src/App.js
+++ src/App.js
@@ -1,118 +1,124 @@
+import React, { useState } from "react";
 import "./App.css";
-import { React, useState } from "react";
 
-function App() {
+function Modal({ title, color, setTitle, index }) {
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        fontSize: "18px",
+        color: "white",
+        margin: "30px 20px",
+        height: "300px",
+        backgroundColor: `${color}`,
+      }}
+    >
+      <h4>{title[index]}</h4>
+      <p>날짜</p>
+      <p>상세 내용</p>
+      <button
+        onClick={() => {
+          const newTitles = [...title];
+          newTitles[0] = "여자 코트추천";
+          setTitle(newTitles);
+        }}
+      >
+        변경
+      </button>
+    </div>
+  );
+}
+
+export default function App() {
   const [title, setTitle] = useState([
     "남자 코트 추천",
     "강남 우동맛집",
     "파이썬 독학",
   ]);
 
-  const [date, setDate] = useState([
-    "1월1일 발행",
-    "11월12일 발행",
-    "3월7일 발행",
-  ]);
-
   const [like, setLike] = useState([0, 0, 0]);
   const [modal, setModal] = useState(false);
-  const [name, setName] = useState(0);
-  const [input, setInput] = useState("");
+  const [seleteNum, setSeleteNum] = useState(null);
+  const [input, setInput] = useState();
 
   return (
-    <>
-      <div className="App">
-        <div className="black-nav">
-          <h4>React Blog</h4>
-        </div>
-        {title.map((item, index) => {
-          return (
-            <div className="list" key={index}>
-              <h4
-                onClick={() => {
-                  {
-                    setName(index);
-                    setModal(!modal);
-                  }
-                }}
-              >
-                {item}
-              </h4>
+    <div className="App">
+      <div className="black-nav">
+        <h4 style={{ fontSize: "18px" }}>ReactBlog </h4>
+      </div>
+
+      <button
+        onClick={() => {
+          const newTitles = [...title].sort();
+          setTitle(newTitles);
+        }}
+      >
+        정렬
+      </button>
+      {title.map((item, index) => {
+        return (
+          <div className="list" key={index}>
+            <h4
+              className="title"
+              onClick={() => {
+                setSeleteNum(index);
+                setModal(seleteNum !== index || !modal);
+              }}
+            >
+              {item}
               <span
-                onClick={() => {
-                  let copy = [...like];
-                  copy[index] = copy[index] + 1;
-                  setLike(copy);
+                onClick={(e) => {
+                  e.stopPropagation();
+                  const newLike = [...like];
+                  newLike[index] += 1;
+                  setLike(newLike);
                 }}
               >
                 👍
               </span>
-              <span>{like[index]}</span>
-
-              <p>{date[index]}</p>
-              <button
-                onClick={() => {
-                  let copy = [...title];
-                  copy.splice(index, 1);
-                  setTitle(copy);
-                }}
-              >
-                삭제
-              </button>
-            </div>
-          );
-        })}
-        <input
-          type="text"
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
-          placeholder="ex ) 글 제목"
-        ></input>
-
-        <button
-          onClick={() => {
-            if (input == "") {
-              return;
-            }
-            let dates = [...date];
-            let likes = [...like];
-            let titles = [...title];
-            titles.unshift(input);
-            dates.unshift(
-              `${new Date().getMonth()}월${new Date().getDay()}일 발행`
-            );
-            likes.unshift(0);
-            setTitle(titles);
-            setLike(likes);
-            setDate(dates);
-          }}
-        >
-          발행
-        </button>
-        {modal ? <Modal title={title} setTitle={setTitle} name={name} /> : null}
-      </div>
-    </>
-  );
-}
-
-function Modal({ title, setTitle, name }) {
-  return (
-    <div className="modal">
-      <h4>{title[name]}</h4>
-      <p>날짜</p>
-      <p>상세내용</p>
+              {like[index]}
+            </h4>
+            <p>2월 17일 발행</p>
+            <button
+              onClick={() => {
+                const newTitles = [...title];
+                const newLikes = [...like];
+                newTitles.splice(index, 1);
+                newLikes.splice(index, 1);
+                setTitle(newTitles);
+                setLike(newLikes);
+              }}
+            >
+              삭제
+            </button>
+          </div>
+        );
+      })}
+      <input
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      ></input>
       <button
         onClick={() => {
-          let name = [...title];
-          name[0] = "여자코트 추천";
-          setTitle(name);
+          const newTitles = [...title];
+          const newLikes = [...like];
+          newLikes.push(0);
+          newTitles.push(input);
+          setTitle(newTitles);
+          setLike(newLikes);
         }}
       >
-        글 수정
+        버튼
       </button>
+      {modal ? (
+        <Modal
+          color={"black"}
+          title={title}
+          setTitle={setTitle}
+          index={seleteNum}
+        />
+      ) : null}
     </div>
   );
 }
-
-export default App;
